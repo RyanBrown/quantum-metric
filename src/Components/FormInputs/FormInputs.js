37e4fe02b218005f96e334ebc3f6d8d084e02215
Ryan Brown <ryanbrown80@gmail.com>
2020-11-03 18:16:03 -0600

    Quantim Metric - reset button added

---
+++ src/Components/FormInputs/FormInputs.js
@@ -0,0 +1,153 @@
+import React from 'react';
+import SelectMenu from '../SelectMenu/SelectMenu';
+import TextInput from '../TextInput/TextInput';
+import useInput from './useInputHook';
+import './styles.scss';
+
+// Set up form block wrappers
+const Marker = (props) => {
+    return <span className='marker'>{props.variant}</span>;
+};
+
+export default function FormInputs() {
+    const { value: id, bind: bindId, reset: resetId } = useInput('');
+    const { value: userEmail, bind: bindUserEmail, reset: resetUserEmail } = useInput('');
+    const { value: userFirstName, bind: bindUserFirstName, reset: resetUserFirstName } = useInput('');
+    const { value: userLastName, bind: bindUserLastName, reset: resetUserLastName } = useInput('');
+    const { value: screenWidthMin, bind: bindScreenWidthMin, reset: resetScreenWidthMin } = useInput('');
+    const { value: screenWidthMax, bind: bindScreenWidthMax, reset: resetScreenWidthMax } = useInput('');
+    const { value: screenHeightMin, bind: bindScreenHeightMin, reset: resetScreenHeightMin } = useInput('');
+    const { value: screenHeightMax, bind: bindScreenHeightMax, reset: resetScreenHeightMax } = useInput('');
+    const { value: visits, bind: bindVisits, reset: resetVisits } = useInput('');
+    const { value: pageResponse, bind: bindPageResponse, reset: resetPageResponse } = useInput('');
+    const { value: domain, bind: bindDomain, reset: resetDomain } = useInput('');
+    const { value: path, bind: bindPath, reset: resetPath } = useInput('');
+    const { value: predicatedOptions, bind: bindPredicatedOptions, reset: resetPredicatedOptions } = useInput('');
+    const { value: stringOptions, bind: bindStringOptions, reset: resetStringOptions } = useInput('');
+    const { value: integerOptions, bind: bindIntegerOptions, reset: resetIntegerOptions } = useInput('');
+
+    const isMarker = <Marker variant='is' />;
+    const andMarker = <Marker variant='and' />;
+
+    const inputId = <TextInput type='text' {...bindId} placeholder='id' />;
+    const inputEmail = <TextInput type='email' {...bindUserEmail} placeholder='Email' />;
+    const inputFirstName = <TextInput type='text' {...bindUserFirstName} placeholder='First Name' />;
+    const inputLastName = <TextInput type='text' {...bindUserLastName} placeholder='Last Name' />;
+    const inputScreenWidthMin = <TextInput type='number' {...bindScreenWidthMin} placeholder='Screen Width' />;
+    const inputScreenWidthMax = <TextInput type='number' {...bindScreenWidthMax} placeholder='Screen Width' />;
+    const inputScreenHeightMin = <TextInput type='number' {...bindScreenHeightMin} placeholder='Screen Height' />;
+    const inputScreenHeightMax = <TextInput type='number' {...bindScreenHeightMax} placeholder='Screen Height' />;
+    const inputVisits = <TextInput type='text' {...bindVisits} placeholder='Visits' />;
+    const inputPageResponse = <TextInput type='text' {...bindPageResponse} placeholder='Page Response' />;
+    const inputDomain = <TextInput type='url' {...bindDomain} placeholder='Domain' />;
+    const inputPath = <TextInput type='text' {...bindPath} placeholder='Path' />;
+
+    const predicatedMenu = (
+        <SelectMenu {...bindPredicatedOptions}>
+            <option>Select Option</option>
+            <option value='id'>id</option>
+            <option value='Email'>User Email</option>
+            <option value='First_Name'>First Name</option>
+            <option value='Last_Name'>Last Name</option>
+            <option value='Screen_Width'>Screen Width</option>
+            <option value='Screen_Height'>Screen Height</option>
+            <option value='Num_Of_Visits'># of Visits</option>
+            <option value='Page_Response'>Page Response time (ms)</option>
+            <option value='Domain'>Domain</option>
+            <option value='Page_Path'>Page Path</option>
+        </SelectMenu>
+    );
+
+    const integerMenu = (
+        <SelectMenu {...bindIntegerOptions}>
+            <option>Select Operator(s)</option>
+            <option value='='>equals</option>
+            <option value='BETWEEN'>between</option>
+            <option value='>'>greater than</option>
+            <option value='<'>less than</option>
+            <option value='IN'>in list</option>
+        </SelectMenu>
+    );
+
+    const stringMenu = (
+        <SelectMenu {...bindStringOptions}>
+            <option>Select Operator(s)</option>
+            <option value='='>equals</option>
+            <option value='CONTAINS'>contains</option>
+            <option value='%STARTSWITH'>starts with</option>
+            <option value='IN'>in list</option>
+        </SelectMenu>
+    );
+
+    return (
+        <>
+            {predicatedMenu}
+            {(() => {
+                switch (predicatedOptions) {
+                    case 'userEmail':
+                        return (
+                            <>
+                                {stringMenu} {inputEmail}
+                            </>
+                        );
+                    case 'screenWidth':
+                        return (
+                            <>
+                                {isMarker} {integerMenu} {inputScreenWidthMin} {andMarker} {inputScreenWidthMax}
+                            </>
+                        );
+                    case 'screenHeight':
+                        return (
+                            <>
+                                {isMarker} {integerMenu} {inputScreenHeightMin} {andMarker}
+                                {inputScreenHeightMax}
+                            </>
+                        );
+                    case 'visits':
+                        return (
+                            <>
+                                {isMarker} {integerMenu} {inputVisits} {andMarker}
+                                {inputVisits}
+                            </>
+                        );
+                    case 'userFirstName':
+                        return (
+                            <>
+                                {stringMenu} {inputFirstName}
+                            </>
+                        );
+                    case 'userLastName':
+                        return (
+                            <>
+                                {stringMenu} {inputLastName}
+                            </>
+                        );
+                    case 'pageResponse':
+                        return (
+                            <>
+                                {isMarker} {integerMenu} {inputPageResponse} {andMarker} {inputPageResponse}
+                            </>
+                        );
+                    case 'domain':
+                        return (
+                            <>
+                                {stringMenu} {inputDomain}
+                            </>
+                        );
+                    case 'pagePath':
+                        return (
+                            <>
+                                {stringMenu} {inputPath}
+                            </>
+                        );
+                    default:
+                        return (
+                            <>
+                                {stringMenu} {inputId}
+                            </>
+                        );
+                }
+            })()}
+        </>
+    );
+}
---
+++ src/Components/MyForm/MyForm.js
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import Button from '../Button/Button';
+import FormInputs from '../FormInputs/FormInputs';
+import './styles.scss';
+
+// Set up form block wrappers
+const FormBlock = (props) => {
+    return <div className='filter-block'>{props.children}</div>;
+};
+
+// Add/Remove blocks for form inputs
+export default function MyForm(props) {
+    const [filterBlocks, setBlock] = useState([0]);
+    const [block, setNewBlock] = useState('');
+
+    const addItems = () => {
+        setBlock((filterBlockList) => {
+            return [...filterBlockList, block];
+        });
+        setNewBlock('');
+    };
+
+    const removeItems = (id) => {
+        setBlock((filterBlockList) => {
+            return filterBlockList.filter((arrElem, index) => {
+                return index !== (id || id !== [0]);
+            });
+        });
+    };
+
+    // Form
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Submit Clicked');
+    };
+
+    const handleReset = (e) => {
+        e.preventDefault();
+        console.log('Reset Clicked');
+    };
+
+    return (
+        <>
+            <form onSubmit={handleSubmit} onReset={handleReset}>
+                {filterBlocks.map((block, index) => {
+                    return (
+                        <FormBlock key={index}>
+                            <Button
+                                key={index}
+                                id={index}
+                                onClick={() => removeItems(index)}
+                                type='reset'
+                                variant='close'
+                            >
+                                &times;
+                            </Button>
+                            <FormInputs />
+                        </FormBlock>
+                    );
+                })}
+                <Button variant='primary' onClick={addItems}>
+                    And
+                </Button>
+
+                <div className='action-bar'>
+                    <Button type='submit' variant='primary' size='lg'>
+                        Search
+                    </Button>
+                    <Button type='reset' value='Reset' size='lg'>
+                        Reset
+                    </Button>
+                </div>
+            </form>
+
+            <div className='generated-sql'></div>
+        </>
+    );
+}
